d99ed772d087b404931f85c59cbe430474ad2aaf
Alexandre <alexandree.campo@gmail.com>
2024-05-02 13:59:34 -0300

    first commit

--- Projetinho/obj/Debug/net8.0/Projetinho.AssemblyInfo.cs
+++ Projetinho/obj/Debug/net8.0/Projetinho.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projetinho")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a9b3da3f70edc474ead432d6e211779b0cbee1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projetinho")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projetinho")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
