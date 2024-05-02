abd288547768d1c0c6a8fbe2e63d0968314b3062
Alexandre <alexandree.campo@gmail.com>
2024-05-02 14:34:55 -0300

    update
    refactoring

--- contentContext/Course.cs
+++ contentContext/Course.cs
@@ -12,25 +12,4 @@ namespace Projetinho.ContentContext
         public IList<Module> Modules { get; set; }
     }
 
-    public class Module //List
-    {
-        public Course ()
-        {
-            Lectures = new List<Lecture>();
-        }
-
-        public int Order { get; set; }
-
-        public string Title { get; set; }
-
-        public IList<Lecture> Lecture { get; set; }
-
-    }
-
-    public class Lecture
-    {
-        public int Order { get; set; }
-
-        public string Title { get; set; }
-    }
 }
